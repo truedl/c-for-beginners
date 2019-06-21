5f16f7572a41a05517da2ea0ebc3704c4baec25f
Tom <25082829+truedl@users.noreply.github.com>
2019-06-21 16:31:26 +0300

    farmer.c (Solution)

---
+++ 0005_The_Farmer/farmer.c
@@ -0,0 +1,19 @@
+// Max the farmer wants to write daily his earnings from selling wheat (Max need to earn minimum 200 coins to end his working day).
+// Write a program for Max that he can write his earnings and stop after he reaches 200+ coins.
+
+#include <stdio.h>
+
+int main(){
+    float slot, earnings = 0;
+
+    while(earnings < 200){
+        printf("Enter earnings: ");
+        scanf("%f", &slot);
+
+        earnings += slot;
+    }
+
+    printf("Congratulations!. You reach %.3f coins and now you can take a break.", earnings);
+
+    return 0;
+}
