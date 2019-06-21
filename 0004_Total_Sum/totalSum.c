5cd30cb1b1c78e2e7f73f3c48868caa3006d10f8
Tom <25082829+truedl@users.noreply.github.com>
2019-06-21 16:30:22 +0300

    totalSum.c (Solution)

---
+++ 0004_Total_Sum/totalSum.c
@@ -0,0 +1,16 @@
+// Danny wants to buy X computers for his company, one computer price is 95.125 coins.
+// Write a program that help Danny to calculate it.
+
+#include <stdio.h>
+
+int main(){
+    float computer_price = 92.125;
+    int x;
+
+    printf("Danny, how many computers you want to buy? ");
+    scanf("%d", &x);
+
+    printf("Price of %d computers is %.3f coins.", x, x * computer_price);
+
+    return 0;
+}
