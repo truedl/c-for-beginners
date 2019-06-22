24fab9115255c0893ef09e685faa4ad90f79512d
Tom <25082829+truedl@users.noreply.github.com>
2019-06-22 12:08:02 +0300

    theMarathon.c (Solution)

---
+++ 0008_The_Marathon/theMarathon.c
@@ -0,0 +1,32 @@
+// Your brother Alex is getting ready to participate at the 40km marathon in the next week. He is asking you to write a stopwatch program
+// that he can start it and stop it and check his time in minutes.
+
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+    clock_t start, end;
+    _Bool quit = 0;
+    char c, s;
+
+    while(!quit){
+        printf("Press enter to start or type ANY one character to quit.\n");
+        scanf("%c", &c);
+
+        switch(c){
+            case '\n':
+                start = clock();
+                printf("\nPress enter to stop the stopwatch.\n");
+                scanf("%c", &s);
+                end = clock() - start;
+                printf("Result: %.3f minutes.\n", end/1000.0/100.0);
+                break;
+            
+            default:
+                quit = 1;
+                break;
+        }
+    }
+
+    return 0;
+}
